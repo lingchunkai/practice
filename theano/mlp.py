1c3405c532ae53a551d7aace758ebee6cbe94334
lingchunkai <lingchunkai@gmail.com>
2016-09-09 03:24:30 +0800

    added mlp forward propagation

--- theano/mlp.py
+++ theano/mlp.py
@@ -1,5 +1,5 @@
 import theano
-import theano import function, shared
+from theano import function, shared, pp
 import theano.tensor as T
 import numpy as np
 
@@ -10,6 +10,9 @@ This is just to reinforce understanding of underlying gradient flow, not to repl
 
 NOTE: all 32 bit in case we want to CUDA things in future!
 '''
+theano.config.compute_test_value = "ignore"
+theano.config.exception_verbosity = 'high'
+
 
 class FC_layer:
     def __init__(self, vec_in, n_in, n_out, activation='tanh'):
@@ -21,10 +24,26 @@ class FC_layer:
         # initial weights are selected from N(0, 1)
         initial_weights = np.random.normal(0., 1., [n_out, n_in])
         self.weights = shared(initial_weights)
-        self.bias = shared(np.zeros([n_out]))
+        self.bias = shared(np.zeros([n_out, 1]))
 
-        tform = T.dot(self.weights, self.vec_in)
-        # output = theano.scan(lambda v: T.tanh(tform) + self.bias, sequences=
+        tmp = T.dot(self.weights, self.vec_in) + self.bias
+        self.output = T.tanh(tmp)
 
-def ConnectLayer(inputs, outputs):
-    pass
+
+class MLP:
+    def __init__(self, layer_sizes):
+        self.inputs = T.dmatrix('mlp_inputs')
+        self.output_nodes = [self.inputs]
+        self.layers = []  # excludes first layer
+        for x in xrange(1, len(layer_sizes)):
+            self.layers.append(FC_layer(self.output_nodes[-1], layer_sizes[x-1], layer_sizes[x]))
+            self.output_nodes.append(self.layers[-1].output)
+        self.outputs = self.layers[-1].output
+        self.func = function([self.inputs], [self.outputs])
+
+    def __call__(self, inputs):
+        return self.func(inputs)
+
+mlp = MLP([7,3,6,2])
+print pp(mlp.outputs)
+print mlp(np.array([[1],[2],[3],[4],[5],[6],[7]]))
